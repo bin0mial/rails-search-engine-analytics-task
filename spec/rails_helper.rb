e798529e955ee59004e5fdc4855c1c497a5d35bf
John Eskandar <johng.eskandar@gmail.com>
2024-01-11 15:57:32 +0200

    add factory bot and shoulda matcher

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,9 +3,13 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort 'The Rails environment is running in production mode!' if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'support/factory_bot'
+require 'shoulda/matchers'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -60,4 +64,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Rails.application.routes.url_helpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
